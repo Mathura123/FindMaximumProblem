e65ec5b95be6fdf829cb7241662a508dc32d7839
Mathura <mathuradas333@gmail.com>
2020-10-21 16:59:26 +0530

    [Mathura]Test. Tested Given_Max__Value_At_First_Position_Should_Return_Max_Value

--- FindMaximumProblem/GetMaximum.cs
+++ FindMaximumProblem/GetMaximum.cs
@@ -5,33 +5,6 @@ namespace FindMaximumProblem
 {
     public class GetMaximum
     {
-        public int GetMaxInt(int firstInt, int secondInt, int thirdInt)
-        {
-            if (firstInt.CompareTo(secondInt) >= 0 && firstInt.CompareTo(thirdInt) >= 0)
-                return firstInt;
-            else if (secondInt.CompareTo(firstInt) >= 0 && secondInt.CompareTo(thirdInt) >= 0)
-                return secondInt;
-            else
-                return thirdInt;
-        }
-        public double GetMaxFloat(double firstFloat, double secondFloat, double thirdFloat)
-        {
-            if (firstFloat.CompareTo(secondFloat) >= 0 && firstFloat.CompareTo(thirdFloat) >= 0)
-                return firstFloat;
-            else if (secondFloat.CompareTo(firstFloat) >= 0 && secondFloat.CompareTo(thirdFloat) >= 0)
-                return secondFloat;
-            else
-                return thirdFloat;
-        }
-        public string GetMaxString(string firstString, string secondString, string thirdString)
-        {
-            if (firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) >= 0)
-                return firstString;
-            else if (secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) >= 0)
-                return secondString;
-            else
-                return thirdString;
-        }
         public T GetMaxValue<T>(T firstValue, T secondValue, T thirdValue) where T : IComparable
         {
             if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
--- FindMaximumUTest/UnitTests.cs
+++ FindMaximumUTest/UnitTests.cs
@@ -7,101 +7,13 @@ namespace FindMaximumUTest
     public class UnitTests
     {
         [TestMethod]
-        public void Given_Max_Number_At_First_Position_Should_Return_The_Max_No()
-        {
-            //Arrange
-            int expected = 90;
-            GetMaximum getMaxTestObj = new GetMaximum();
-            //Act
-            int result = getMaxTestObj.GetMaxInt(90, 8, 45);
-            //Assert
-            Assert.AreEqual(expected, result);
-        }
-        [TestMethod]
-        public void Given_Max_Number_At_Second_Position_Should_Return_The_Max_No()
-        {
-            //Arrange
-            int expected = 90;
-            GetMaximum getMaxTestObj = new GetMaximum();
-            //Act
-            int result = getMaxTestObj.GetMaxInt(5, 90, 45);
-            //Assert
-            Assert.AreEqual(expected, result);
-        }
-        [TestMethod]
-        public void Given_Max_Number_At_Third_Position_Should_Return_The_Max_No()
-        {
-            //Arrange
-            int expected = 90;
-            GetMaximum getMaxTestObj = new GetMaximum();
-            //Act
-            int result = getMaxTestObj.GetMaxInt(9, 80, 90);
-            //Assert
-            Assert.AreEqual(expected, result);
-        }
-        [TestMethod]
-        public void Given_Max_Float_No_At_First_Position_Should_Return_The_Max_No()
-        {
-            //Arrange
-            double expected = 90.45;
-            GetMaximum getMaxTestObj = new GetMaximum();
-            //Act
-            double result = getMaxTestObj.GetMaxFloat(90.45, 90.1, 80.45);
-            //Assert
-            Assert.AreEqual(expected, result);
-        }
-        [TestMethod]
-        public void Given_Max_Float_No_At_Second_Position_Should_Return_The_Max_No()
-        {
-            //Arrange
-            double expected = 100.485;
-            GetMaximum getMaxTestObj = new GetMaximum();
-            //Act
-            double result = getMaxTestObj.GetMaxFloat(90.45, 100.485, 80.45);
-            //Assert
-            Assert.AreEqual(expected, result);
-        }
-        [TestMethod]
-        public void Given_Max_Float_No_At_Third_Position_Should_Return_The_Max_No()
-        {
-            //Arrange
-            double expected = 150.455;
-            GetMaximum getMaxTestObj = new GetMaximum();
-            //Act
-            double result = getMaxTestObj.GetMaxFloat(90.45, 90.1, 150.455);
-            //Assert
-            Assert.AreEqual(expected, result);
-        }
-        [TestMethod]
-        public void Given_Max_String_At_First_Position_Should_Return_The_Max_String()
+        public void Given_Max_Value_At_Fiirst_Position_Should_Return_The_Max_Value()
         {
             //Arrange
             string expected = "Peach";
             GetMaximum getMaxTestObj = new GetMaximum();
             //Act
-            string result = getMaxTestObj.GetMaxString("Peach", "Banana", "Apple");
-            //Assert
-            Assert.AreEqual(expected, result);
-        }
-        [TestMethod]
-        public void Given_Max_String_At_Second_Position_Should_Return_The_Max_String()
-        {
-            //Arrange
-            string expected = "Peach";
-            GetMaximum getMaxTestObj = new GetMaximum();
-            //Act
-            string result = getMaxTestObj.GetMaxString("Banana", "Peach", "Apple");
-            //Assert
-            Assert.AreEqual(expected, result);
-        }
-        [TestMethod]
-        public void Given_Max_String_At_Third_Position_Should_Return_The_Max_String()
-        {
-            //Arrange
-            string expected = "Peach";
-            GetMaximum getMaxTestObj = new GetMaximum();
-            //Act
-            string result = getMaxTestObj.GetMaxString("Apple", "Banana", "Peach");
+            string result = getMaxTestObj.GetMaxValue("Peach", "Banana", "Apple");
             //Assert
             Assert.AreEqual(expected, result);
         }
