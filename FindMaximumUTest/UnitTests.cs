b27a05a9a6ea833fa7cc149386d4f79a5d5e367f
Mathura <mathuradas333@gmail.com>
2020-10-21 16:08:36 +0530

    [Mathura]Test. Tested Given_Max_Number_At_Second_Position_Should_eturn_Max_NO

--- FindMaximumUTest/UnitTests.cs
+++ FindMaximumUTest/UnitTests.cs
@@ -7,13 +7,24 @@ namespace FindMaximumUTest
     public class UnitTests
     {
         [TestMethod]
-        public void Given_Max_Number_At_First_Position_Return_The_Max_No()
+        public void Given_Max_Number_At_First_Position_Should_Return_The_Max_No()
         {
             //Arrange
             int expected = 90;
             GetMaximum getMaxTestObj = new GetMaximum();
             //Act
             int result = getMaxTestObj.GetMaxInt(90, 8, 45);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void Given_Max_Number_At_Second_Position_Should_Return_The_Max_No()
+        {
+            //Arrange
+            int expected = 90;
+            GetMaximum getMaxTestObj = new GetMaximum();
+            //Act
+            int result = getMaxTestObj.GetMaxInt(5, 90, 45);
             //Assert
             Assert.AreEqual(expected, result);
         }
