1f499447e863f4bf1f514dca42bf234f122a30f2
Mathura <mathuradas333@gmail.com>
2020-10-21 17:50:01 +0530

    [Mathura]Test. Tested Given_Input_Array_Of_Values_Should_Return_Max_Value_Using_Generic_Class

--- FindMaximumUTest/UnitTests.cs
+++ FindMaximumUTest/UnitTests.cs
@@ -7,38 +7,16 @@ namespace FindMaximumUTest
     public class UnitTests
     {
         [TestMethod]
-        public void Given_Max_Value_At_First_Position_Should_Return_The_Max_Value_Using_Generic_Class()
+        public void Given_Input_Array_Of_Values_Return_The_Max_Value_Using_Generic_Class()
         {
             //Arrange
-            string[] input = { "Peach", "Banana", "Apple","Orange"};
-            string expected = "Peach";
-            GetMaximum<string> getMaxTestObj = new GetMaximum<string>(input);
+            int[] input = { 45, 389, 41 ,45,589,35};
+            int expected = 589;
+            GetMaximum<int> getMaxTestObj = new GetMaximum<int>(input);
             //Act
-            string result = getMaxTestObj.GetMaxValue();
+            int result = getMaxTestObj.GetMaxValue();
             //Assert
             Assert.AreEqual(expected, result);
         }
-        //[TestMethod]
-        //public void Given_Max_Value_At_Second_Position_Should_Return_The_Max_Value_Using_Generic_Class()
-        //{
-        //    //Arrange
-        //    int expected = 389;
-        //    GetMaximum<int> getMaxTestObj = new GetMaximum<int>(45, 389, 41);
-        //    //Act
-        //    int result = getMaxTestObj.GetMaxValue();
-        //    //Assert
-        //    Assert.AreEqual(expected, result);
-        //}
-        //[TestMethod]
-        //public void Given_Max_Value_At_Third_Position_Should_Return_The_Max_Value_Using_Generic_Class()
-        //{
-        //    //Arrange
-        //    double expected = 145.01;
-        //    GetMaximum<double> getMaxTestObj = new GetMaximum<double>(45.12, 7.152, 145.01);
-        //    //Act
-        //    double result = getMaxTestObj.GetMaxValue();
-        //    //Assert
-        //    Assert.AreEqual(expected, result);
-        //}
     }
 }
